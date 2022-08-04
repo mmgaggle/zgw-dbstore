08a19390f27459b0b2a8e91dc38becf0738374f3
kbader <kbader@redhat.com>
2022-08-04 16:25:20 -0700

    Updates

--- rgw-dbstore/README.md
+++ rgw-dbstore/README.md
@@ -7,6 +7,27 @@ podman build -t rgw-dbstore ./
 # Running with Podman
 
 ```
-podman run -it rgw-dbstore:latest -v /mnt:/var/lib/ceph
+podman run -it rgw-dbstore:latest \
+  -v /mnt:/var/lib/ceph \
+  -e ACCESS_KEY=$ACCESS_KEY \
+  -e SECRET_KEY=$SECRET_KEY
 ```
 
+# Enter zgw container
+
+```
+podman exec -it <container id> /bin/bash
+```
+
+# Create a bucket
+```
+s5cmd --endpoint-url http://127.0.0.1:7480 mb s3://warp-benchmark
+```
+
+# Run warp benchmark
+```
+warp put --host 127.0.0.1:7480 \
+         --access-key zippy \
+         --secret-key zippy \
+         --duration 10s
+```
--- rgw-dbstore/entrypoint.sh
+++ rgw-dbstore/entrypoint.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 set -e
 
+radosgw-admin user create \
+  --uid zippy \
+  --display-name zippy \
+  --access-key ${ACCESS_KEY:-zippy} \
+  --secret-key ${SECRET_KEY:-zippy}
+
+cat << EOF > /root/.aws/credentials
+[default]
+aws_access_key_id = ${ACCESS_KEY:-zippy}
+aws_secret_access_key = ${SECRET_KEY:-zippy}
+EOF
+
 cd /var/lib/ceph
 
-/usr/bin/radosgw --cluster ceph --setuser root --setgroup root --default-log-to-stderr=true --err-to-stderr=true --default-log-to-file=false --foreground -n client.rgw --no-mon-config
+/usr/bin/radosgw \
+  --cluster ceph \
+  --setuser root \
+  --setgroup root \
+  --default-log-to-stderr=true \
+  --err-to-stderr=true \
+  --default-log-to-file=false \
+  --foreground \
+  -n client.rgw \
+  --no-mon-config
